7f0361a593160d6cf78e768df2abba253f0a3f29
Hunor Karamán <hunix94@gmail.com>
2018-05-13 15:54:45 +0300

    default to .chat

--- input.js
+++ input.js
@@ -32,31 +32,32 @@ function readInput(db, nick) {
 
     var pattern = (/\.(\w*)\s*(.*)/)
     rl.on("line", function(line) {
-        var match = pattern.exec(line) 
+        var match = pattern.exec(line)
         var cmd = match && match[1] || ""
         var arg = match && match[2] || ""
+
         if (cmd === "put") {
             var [key, value] = arg.split("=")
             db.put(key, value, util.log)
-        } else if (cmd === "test") { 
+        } else if (cmd === "test") {
             interface.test()
-        } else if (cmd === "list") { 
+        } else if (cmd === "list") {
             interface.getChannels().then((channels) => {
                 channels.map(screen.writeLine)
             })
-        } else if (cmd === "chat") { 
+        } else if (cmd === "chat") {
             var channel = `channels/${currentChannel}`
             interface.writeMessage(channel, nick, arg)
-        } else if (cmd === "nick") { 
+        } else if (cmd === "nick") {
             nick = arg
         } else if (cmd === "get") { db.get(arg, util.log)
-        } else if (cmd === "all") { 
+        } else if (cmd === "all") {
             var stream = db.createHistoryStream()
             stream.pipe(through.obj(function(chunk, enc, next) {
                 console.log(chunk)
                 next()
             }))
-        } else if (cmd === "change") { 
+        } else if (cmd === "change") {
             currentChannel = arg
             monitor(`/channels/${currentChannel}`)
             console.log("\033[2J") // hack: clear screen
@@ -64,7 +65,7 @@ function readInput(db, nick) {
             interface.getMessages(channel, 25).then((msg) => {
                 msg.map(screen.writeMessage)
             })
-        } else if (cmd === "read") { 
+        } else if (cmd === "read") {
             var channel = `channels/${currentChannel}`
             interface.getMessages(channel, 20).then((messages) => {
                 messages.map(screen.writeMessage)
@@ -72,7 +73,11 @@ function readInput(db, nick) {
         } else if (cmd === "auth") { db.authorize(Buffer.from(arg, "hex"), util.log)
         } else if (cmd === "local") { console.log("local key is\n\t", db.local.key.toString("hex"))
         } else if (cmd === "db") { console.log("db key is\n\t", db.key.toString("hex"))
-        } else if (cmd === "registered") { db.authorized(Buffer.from(arg, "hex"), util.log) }
+		} else if (cmd === "registered") { db.authorized(Buffer.from(arg, "hex"), util.log)
+		} else {
+			var channel = `channels/${currentChannel}`
+            interface.writeMessage(channel, nick, line)
+		}
     })
 }
 
