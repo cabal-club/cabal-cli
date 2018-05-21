b9431da4729778b6780c9e8401490cbbea9b4aa0
cblgh <cblgh@cblgh.org>
2018-05-21 12:27:05 +0200

    fix a bug with the command regex and refactored

--- commands.js
+++ commands.js
@@ -1,12 +1,61 @@
-var util = require('./util.js')
-var through = require('through2')
-
 function Commander (view, cabal) {
   if (!(this instanceof Commander)) return new Commander(view, cabal)
   this.cabal = cabal
   this.channel = 'default'
   this.view = view
-  this.pattern = (/\/(\w*)\s*(.*)/)
+  this.pattern = (/^\/(\w*)\s*(.*)/)
+
+  var self = this
+  this.commands = {
+    nick: {
+      help: () => 'change your display name',
+      call: (arg) => {
+        if (arg === '') return
+        self.cabal.username = arg
+        self.view.writeLine("* you're now known as " + arg)
+      }
+    },
+    quit: {
+      help: () => 'exit the cabal process',
+      call: (arg) => {
+        process.exit(0)
+      }
+    },
+    list: {
+      help: () => "display the cabal's channels",
+      call: (arg) => {
+        self.cabal.getChannels((err, channels) => {
+          if (err) return
+          self.view.writeLine('* channels:')
+          channels.map((m) => {
+            self.view.writeLine.bind(self.view)(`  ${m}`)
+          })
+        })
+      }
+    },
+    clear: {
+      help: () => 'clear the current backscroll',
+      call: (arg) => {
+        self.view.clear()
+      }
+    },
+    change: {
+      help: () => 'change to a new channel',
+      call: (arg) => {
+        if (arg === '') arg = 'default'
+        self.channel = arg
+        self.view.loadChannel(arg)
+      }
+    },
+    help: {
+      help: () => 'display this help message',
+      call: (arg) => {
+        for (var key in self.commands) {
+          self.view.writeLine(`/${key}\n  ${self.commands[key].help()}`)
+        }
+      }
+    }
+  }
 }
 
 Commander.prototype.process = function (line) {
@@ -15,47 +64,10 @@ Commander.prototype.process = function (line) {
   var cmd = match ? match[1] : ''
   var arg = match ? match[2] : ''
   arg = arg.trim()
-
-  if (cmd === 'put') {
-    var [key, value] = arg.split('=')
-    self.cabal.db.put(key, value, util.log)
-  } else if (cmd === 'list') {
-    self.cabal.getChannels((err, channels) => {
-      if (err) return
-      self.view.writeLine('* channels:')
-      channels.map(self.view.writeLine.bind(self.view))
-    })
-  } else if (cmd === 'nick') {
-    if (arg === '') return
-    this.cabal.username = arg
-    self.view.writeLine("* you're now known as " + arg)
-  } else if (cmd === 'clear') {
-    self.view.clear()
-  } else if (cmd === 'get') {
-    self.cabal.db.get(arg, console.log)
-  } else if (cmd === 'all') {
-    var stream = self.cabal.db.createHistoryStream()
-    stream.pipe(through.obj(function (chunk, enc, next) {
-      console.log(chunk)
-      next()
-    }))
-  } else if (cmd === 'quit') {
-    process.exit(0)
-  } else if (cmd === 'change') {
-    if (arg === '') arg = 'default'
-    self.channel = arg
-    self.view.loadChannel(arg)
-  } else if (cmd === 'messages') {
-    // debug command
-    console.log(self.view.state.messages)
-  } else if (cmd === 'auth') {
-    self.cabal.db.authorize(Buffer.from(arg, 'hex'), util.log)
-  } else if (cmd === 'local') {
-    self.view.writeLine('local: ' + self.cabal.db.local.key.toString('hex'))
-  } else if (cmd === 'db') {
-    self.view.writeLine('db:' + self.cabal.db.key.toString('hex'))
-  } else if (cmd === 'registered') {
-    self.cabal.db.authorized(Buffer.from(arg, 'hex'), util.log)
+  if (cmd in self.commands) {
+    self.commands[cmd].call(arg)
+  } else if (cmd) {
+    self.view.writeLine(`${cmd} is not a command, type /help for commands`)
   } else {
     line = line.trim()
     if (line !== '') {
