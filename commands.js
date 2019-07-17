ec754dfadf912dc4d395833b01728debd3ecd151
cblgh <cblgh@cblgh.org>
2019-07-17 22:13:13 +0200

    fix titled links, replace terminal-link dep

--- commands.js
+++ commands.js
@@ -151,15 +151,13 @@ Commander.prototype.process = function (line) {
   var cmd = match ? match[1] : ''
   var arg = match ? match[2] : ''
   arg = arg.trim()
-  if (self.channel === '!status') {
-    self.view.writeLine(line)
-  } else if (cmd in self.commands) {
+  if (cmd in self.commands) {
     self.commands[cmd].call(arg)
   } else if (cmd) {
     self.view.writeLine(`${cmd} is not a command, type /help for commands`)
   } else {
     line = line.trim()
-    if (line !== '') {
+    if (line !== '' && channel !== "!status") {
       self.cabal.publish({
         type: 'chat/text',
         content: {
