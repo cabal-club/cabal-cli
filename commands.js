faad56e629b6010e41ea755bfd659da2fd6e3924
Alexander Cobleigh <cblgh@cblgh.org>
2020-10-09 16:50:33 +0200

    fix coloring of channels command

--- commands.js
+++ commands.js
@@ -18,7 +18,20 @@ Commander.prototype.setActiveCabal = function (cabal) {
   if (this._hasListeners[cabal.key]) return
   this.cabal.on('info', (msg) => {
     var txt = typeof msg === 'string' ? msg : (msg && msg.text ? msg.text : '')
-    this.view.writeLine('* ' + util.sanitizeString(txt))
+    txt = util.sanitizeString(txt)
+    if (msg.command) {
+      switch (msg.command) {
+          case "channels":
+            var {joined, channel, userCount, topic} = msg
+            var userPart = userCount ? `: ${userCount} ${userCount === 1 ? 'person' : 'people'}` : ''
+            var shortTopic = topic.length > 40 ? topic.slice(0, 40) + '..' : topic || ''
+            shortTopic = util.sanitizeString(shortTopic)
+            channel = util.sanitizeString(channel)
+            txt = `${joined ? '*' : ' '} ${channel}${userPart} ${chalk.cyan(shortTopic)}`
+            break
+      }
+    }
+    this.view.writeLine(txt)
   })
   this.cabal.on('error', (err) => {
     this.view.writeLine(chalk.bold(chalk.red('! ' + util.sanitizeString(String(err)))))
